8c31dc1773f30f23541d8823e804a6ecdec652e3
singhdevendrajtz <singhdevendrajtz@gmail.com>
2025-09-29 17:59:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 // Your code here.
-let isDragging = false;
-let offsetX, offsetY; 
+let isDragging = [false,false,false,false,false,false];
+let offsetX=new Array(6), offsetY=new Array(6); 
 const boxes=document.querySelectorAll(".cubes")
 boxes.forEach((x,y)=>{
-	boxes[y].addEventListener('click',function(){
+	
 	boxes[y].addEventListener('mousedown', (e) => {
                 isDragging = true;
-                offsetX = e.clientX - boxes[y].getBoundingClientRect().left;
-                offsetY = e.clientY - boxes[y].getBoundingClientRect().top;
-    console.log(offsetX,offsetY)       
+                offsetX[y] = e.clientX - boxes[y].getBoundingClientRect().left;
+                offsetY[y] = e.clientY - boxes[y].getBoundingClientRect().top;
+       
 	});
-	document.querySelector(".items").addEventListener('mousemove', (e) => {
+	document.addEventListener('mousemove', (e) => {
                 if (!isDragging) return
 				
-                boxes[y].style.left = (e.clientX - offsetX) + 'px';
-                boxes[y].style.top = (e.clientY - offsetY) + 'px';
+                boxes[y].style.left = (e.clientX - offsetX[y]) + 'px';
+                boxes[y].style.top = (e.clientY - offsetY[y]) + 'px';
             });
 	document.addEventListener('mouseup', () => {
                 isDragging = false;
             });
 	
+ boxes[y].addEventListener('dragstart', (e) => {
+        e.preventDefault();
+    });		
 	})
 	
 		
-		
-	})
+	
