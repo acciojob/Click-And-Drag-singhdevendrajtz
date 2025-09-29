954af959fe4e3433b55cb5237dd083f2ebbb948c
singhdevendrajtz <singhdevendrajtz@gmail.com>
2025-09-29 16:13:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // Your code here.
+let isDragging = false;
+let offsetX, offsetY; 
+const boxes=document.querySelectorAll(".cubes")
+boxes.forEach((x,y)=>{
+	boxes[y].addEventListener('click',function(){
+	boxes[y].addEventListener('mousedown', (e) => {
+                isDragging = true;
+                offsetX = e.clientX - boxes[y].getBoundingClientRect().left;
+                offsetY = e.clientY - boxes[y].getBoundingClientRect().top;
+    console.log(offsetX,offsetY)       
+	});
+	document.querySelector(".items").addEventListener('mousemove', (e) => {
+                if (!isDragging) return
+				
+                boxes[y].style.left = (e.clientX - offsetX) + 'px';
+                boxes[y].style.top = (e.clientY - offsetY) + 'px';
+            });
+	document.addEventListener('mouseup', () => {
+                isDragging = false;
+            });
+	
+	})
+	
+		
+		
+	})
